4f075c693125fc9fcb7ba78aa5de8553e2950a1e
vadim.podvoyskiy <vadim.podvoyskiy@gmail.com>
2022-09-27 16:19:32 +0500

    Updated CreateDBandTables

--- Python/CreateDBandTables.py
+++ Python/CreateDBandTables.py
@@ -1,18 +1,13 @@
 import mysql.connector
-import os
-from dotenv import load_dotenv
+import variables as var
 
 
 def db_connection():
-    load_dotenv()
-
-    username = os.environ["$MYSQL_USER"]
-    password = os.environ["$MYSQL_PASSWORD"]
 
     my_db = mysql.connector.connect(
         host="localhost",
-        user=f"{username}",
-        password=f"{password}",
+        user=f"{var.username_db}",
+        password=f"{var.password_db}",
         database="PodFT"
     )
 
@@ -22,15 +17,11 @@ def db_connection():
 
 
 def db_connection_func():
-    load_dotenv()
-
-    username = os.environ["$MYSQL_USER"]
-    password = os.environ["$MYSQL_PASSWORD"]
 
     my_db = mysql.connector.connect(
         host="localhost",
-        user=f"{username}",
-        password=f"{password}"
+        user=f"{var.username_db}",
+        password=f"{var.password_db}"
     )
 
     my_cursor = my_db.cursor()
@@ -39,8 +30,8 @@ def db_connection_func():
 
     my_db = mysql.connector.connect(
         host="localhost",
-        user=f"{username}",
-        password=f"{password}",
+        user=f"{var.username_db}",
+        password=f"{var.password_db}",
         database="PodFT"
     )
 
@@ -51,8 +42,9 @@ def db_connection_func():
 
 def create_octo_table_func(my_cursor):
     sql = "CREATE TABLE Initial_Data_OCTO (octo_trxn_id VARCHAR(255), created_date TIMESTAMP(3), " \
-          "masked_card_number VARCHAR(255), amount DOUBLE(30, 2), currency VARCHAR(255), provider_id VARCHAR(255), " \
-          "dest_tool_id VARCHAR(255), customer_id VARCHAR(255), type_description VARCHAR(255), " \
+          "masked_card_number VARCHAR(255), amount DOUBLE(30, 2), currency VARCHAR(255), " \
+          "transaction_status VARCHAR(255), provider_id VARCHAR(255), dest_tool_id VARCHAR(255), " \
+          "customer_id VARCHAR(255), type_description VARCHAR(255), schema_id VARCHAR(255), " \
           "bill_account_id VARCHAR(255));"
 
     my_cursor.execute(sql)
@@ -60,12 +52,12 @@ def create_octo_table_func(my_cursor):
 
 def create_p2p_table_func(my_cursor):
     sql = "CREATE TABLE Initial_Data_P2P (time_id TIMESTAMP(3), customer_id VARCHAR(255), user_id VARCHAR(255), " \
-          "operation_type VARCHAR(255), amount DOUBLE(30, 2), currency VARCHAR(255), country VARCHAR(255), " \
-          "mcc VARCHAR(255), merch_name VARCHAR(255), masked_card_number VARCHAR(255), card_status VARCHAR(255), " \
-          "fio VARCHAR(255), birth_date VARCHAR(255), citizenship VARCHAR(255), registration_address VARCHAR(255), " \
-          "document_number VARCHAR(255), doc_type VARCHAR(255), pinfl VARCHAR(255), pos_code VARCHAR(255), " \
-          "pos_name VARCHAR(255))"
-
+          "operation_type VARCHAR(255), amount DOUBLE(30, 2), currency_code VARCHAR(255), currency VARCHAR(255), " \
+          "currency_name VARCHAR(255), acq_country_code VARCHAR(255), acq_mcc VARCHAR(255), " \
+          "acq_merch_name VARCHAR(255), country VARCHAR(255), masked_card_number VARCHAR(255), " \
+          "card_status VARCHAR(255), fio VARCHAR(255), birth_date VARCHAR(255), citizenship VARCHAR(255), " \
+          "registration_address VARCHAR(255), document_number VARCHAR(255), doc_type VARCHAR(255), " \
+          "pinfl VARCHAR(255), pos_code VARCHAR(255), pos_name VARCHAR(255))"
     my_cursor.execute(sql)
 
 
@@ -105,32 +97,50 @@ def create_mrot(my_cursor, t):
 
 
 def create_offshore(my_cursor, t):
-    sql = f"CREATE TABLE offshore_{t} (person VARCHAR(255), birthday VARCHAR(255), passport VARCHAR(255), " \
-          "operation_date DATE, amount DOUBLE(30, 2), country VARCHAR(255));"
+    sql = f"CREATE TABLE offshore_{t} (person VARCHAR(255), birthday VARCHAR(255), citizenship VARCHAR(255), " \
+          "registration_address VARCHAR(255), passport VARCHAR(255), operation_date DATE, amount DOUBLE(30, 2), " \
+          "currency VARCHAR(255), country VARCHAR(255), merch_name VARCHAR(255), mcc VARCHAR(255));"
     my_cursor.execute(sql)
 
 
-# cursor, db = db_connection_func()
-cursor, db = db_connection()
+def create_brv(my_cursor, t):
+    sql = f"CREATE TABLE brv_{t} (person VARCHAR(255), birthday VARCHAR(255), citizenship VARCHAR(255), " \
+          "registration_address VARCHAR(255), passport VARCHAR(255), operation_date DATE, amount DOUBLE(30, 2), " \
+          "currency VARCHAR(255), country VARCHAR(255), merch_name VARCHAR(255), mcc VARCHAR(255));"
+    my_cursor.execute(sql)
 
-# create_octo_table_func(cursor)
-# create_p2p_table_func(cursor)
+def create_questionable_operations(my_cursor, t):
+    sql = f"CREATE TABLE questionable_operations_{t} (person VARCHAR(255), birthday VARCHAR(255), " \
+          "citizenship VARCHAR(255), registration_address VARCHAR(255), passport VARCHAR(255), operation_date DATE, " \
+          "amount DOUBLE(30, 2), currency VARCHAR(255), country VARCHAR(255), merch_name VARCHAR(255), " \
+          "mcc VARCHAR(255));"
+    my_cursor.execute(sql)
 
-# create_trans_gran_to_tt(cursor, 'week')
-# create_trans_gran_to_tt(cursor, 'month')
-# create_pinfl_receiver(cursor, 'week')
-# create_pinfl_receiver(cursor, 'month')
-# create_country_p2p(cursor, 'week')
-# create_country_p2p(cursor, 'month')
-create_offshore(cursor, 'week')
-create_offshore(cursor, 'month')
-# create_number_receiver_octo(cursor, 'week')
-# create_number_receiver_octo(cursor, 'month')
-# create_card_sender_octo(cursor, 'week')
-# create_card_sender_octo(cursor, 'month')
 
-# create_mrot(cursor, 'day')
-# create_mrot(cursor, 'week')
-# create_mrot(cursor, 'month')
+cursor, db = db_connection_func()
+# cursor, db = db_connection()
 
+create_octo_table_func(cursor)
+create_p2p_table_func(cursor)
+
+create_trans_gran_to_tt(cursor, 'week')
+create_trans_gran_to_tt(cursor, 'month')
+create_pinfl_receiver(cursor, 'week')
+create_pinfl_receiver(cursor, 'month')
+create_country_p2p(cursor, 'week')
+create_country_p2p(cursor, 'month')
+create_number_receiver_octo(cursor, 'week')
+create_number_receiver_octo(cursor, 'month')
+create_card_sender_octo(cursor, 'week')
+create_card_sender_octo(cursor, 'month')
+create_mrot(cursor, 'day')
+create_mrot(cursor, 'week')
+create_mrot(cursor, 'month')
+
+create_offshore(cursor, 'day')
+
+create_questionable_operations(cursor, 'day')
+
+create_brv(cursor, 'day')
+create_brv(cursor, 'month')
 
